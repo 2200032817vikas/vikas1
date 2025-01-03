ebb60a470f5316e3036912429b3cfc64a5e3e553
2200032817vikas <141983711+2200032817vikas@users.noreply.github.com>
2025-01-03 12:33:35 +0530

    Update java1.java

--- java1.java
+++ java1.java
@@ -2,5 +2,6 @@ public class java1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 	System.out.println("vikas"};
+	System.out.println("harsha"};    
     }
 }
